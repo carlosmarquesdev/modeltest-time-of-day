84f45038f07bc220382e5d0d6efcaaed7e18acda
Carlos Marques <somdeparaiba@gmail.com>
2022-03-25 19:12:25 -0300

    apdate code

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 
-    function carregar(){
-      var msg = window.document.getElementById('msg')
-      var iamgem = window.document.getElementById('imagem')  
+function carregar() {
+  const msg = window.document.getElementById('msg')
+  const img = window.document.getElementById('imagem')
 
-      var data = new Date()
-      var hora = data.getHours()
-      // var hora = 13
+  const data = new Date()
+  const hora = 9
+  const horadia = document.getElementById('hora-dia')
 
-      msg.innerHTML = `São exatamente ${hora} horas`
+  msg.innerHTML = `São exatamente ${hora} horas`
+  horadia.innerHTML = 0
 
-      if (hora >= 0 && hora < 12){
-           img.src = 'manha.jpg'
-           document.body.style.background = '#e2cd9f'
-      }else if (hora >= 12 && hora < 18){
-          img.src = 'tarde.jpj'
-          document.body.style.background = '#b9846f'
-      }else{
-          img.src = 'noite.jpg'
-          document.body.style.background = '#515154'
-      }    
+  if (hora >= 0 && hora < 12) {
+    if (hora < 10) {
+      horadia.innerHTML = 'Bom dia, ja tomou café?'
+    } else {
+      horadia.innerHTML = 'Bom dia!'
+    }
+    img.src = 'manha.jpg'
+    document.body.style.background = '#e2cd9f'
+  } else if (hora >= 12 && hora < 18) {
+    img.src = 'tarde.jpg'
+    document.body.style.background = '#b9846f'
+    horadia.innerHTML = 'Boa tarde!'
+  } else {
+    img.src = 'noite.jpg'
+    document.body.style.background = '#515154'
+    horadia.innerHTML = 'Boa noite!'
+  }
 }
 
 
 
-      
+
